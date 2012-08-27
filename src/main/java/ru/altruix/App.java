b168d86036f254ab960e5d6d57102516b64c036a
Dmitri Pisarenko <dp@altruix.co>
2012-08-27 15:09:59 +0200

    Implemented method ru.altruix.App.readDeathRates(String).

--- src/main/java/ru/altruix/App.java
+++ src/main/java/ru/altruix/App.java
@@ -2,6 +2,7 @@ package ru.altruix;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -63,6 +64,8 @@ public class App {
         try {
             final List<String> lines = IOUtils.readLines(inputStream);
 
+            lines.remove(0); // Remove header
+
             for (final String curLine : lines) {
                 final StringTokenizer tokenizer =
                         new StringTokenizer(curLine, ";");
@@ -70,19 +73,8 @@ public class App {
                 final String idAsString = tokenizer.nextToken();
                 final String ageAsString = tokenizer.nextToken();
 
-                long id = -1;
-                try {
-                    id = Long.parseLong(idAsString);
-                } catch (final NumberFormatException exception) {
-                    LOGGER.error("Cannot parse ID '{}'", idAsString);
-                }
-
-                int age = -1;
-                try {
-                    age = Integer.parseInt(ageAsString);
-                } catch (final NumberFormatException exception) {
-                    LOGGER.error("Cannot parse age '{}'", ageAsString);
-                }
+                long id = parseLongQuietly(idAsString);
+                int age = parseIntQuietly(ageAsString);
 
                 if ((id > 0) && (age >= 0)) {
                     final Human human = new Human();
@@ -101,9 +93,75 @@ public class App {
         return returnValue;
     }
 
+    private int parseIntQuietly(final String aAgeAsString) {
+        int age = -1;
+        try {
+            age = Integer.parseInt(aAgeAsString);
+        } catch (final NumberFormatException exception) {
+            LOGGER.error("Cannot parse integer value '{}'", aAgeAsString);
+        }
+        return age;
+    }
+
+    private long parseLongQuietly(final String aIdAsString) {
+        long id = -1;
+        try {
+            id = Long.parseLong(aIdAsString);
+        } catch (final NumberFormatException exception) {
+            LOGGER.error("Cannot parse long value '{}'", aIdAsString);
+        }
+        return id;
+    }
+
     private Map<Integer, Double> readDeathRates(final String aPath) {
-        // TODO Auto-generated method stub
-        return null;
+        final Map<Integer, Double> returnValue = new HashMap<Integer, Double>();
+        final InputStream inputStream =
+                Thread.currentThread()
+                        .getContextClassLoader()
+                        .getResourceAsStream(
+                                aPath);
+
+        try {
+            final List<String> lines = IOUtils.readLines(inputStream);
+
+            lines.remove(0);
+
+            for (final String curLine : lines) {
+                final StringTokenizer tokenizer =
+                        new StringTokenizer(curLine, ";");
+
+                tokenizer.nextToken(); // Ignore year
+                final String ageAsString = tokenizer.nextToken();
+                tokenizer.nextToken(); // Ignore female death rate
+                tokenizer.nextToken(); // Ignore male death rate
+                final String deathRateAsString = tokenizer.nextToken();
+
+                int age = parseIntQuietly(ageAsString);
+                double deathRate = parseDoubleQuietly(deathRateAsString);
+
+                if ((age >= 0) && (deathRate >= 0.)) {
+                    returnValue.put(age, deathRate);
+                }
+            }
+        } catch (final IOException exception) {
+            LOGGER.error("", exception);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+
+        return returnValue;
+    }
+
+    private double parseDoubleQuietly(final String aDoubleValueAsString) {
+        double age = -1;
+        try {
+            age = Double.parseDouble(aDoubleValueAsString);
+        } catch (final NumberFormatException exception) {
+            LOGGER.error("Cannot parse integer value '{}'",
+                    aDoubleValueAsString);
+        }
+        return age;
+
     }
 
     public static void main(final String[] aArgs) {
