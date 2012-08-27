59bf52c2e48160a58c089e534b94efb658398d44
Dmitri Pisarenko <dp@altruix.co>
2012-08-27 14:31:57 +0200

    Implemented method ru.altruix.App.readPeople(String).

--- src/main/java/ru/altruix/App.java
+++ src/main/java/ru/altruix/App.java
@@ -1,8 +1,14 @@
 package ru.altruix;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,8 +53,52 @@ public class App {
     }
 
     private List<Human> readPeople(final String aPath) {
-        // TODO Auto-generated method stub
-        return null;
+        final List<Human> returnValue = new LinkedList<Human>();
+        final InputStream inputStream =
+                Thread.currentThread()
+                        .getContextClassLoader()
+                        .getResourceAsStream(
+                                aPath);
+
+        try {
+            final List<String> lines = IOUtils.readLines(inputStream);
+
+            for (final String curLine : lines) {
+                final StringTokenizer tokenizer =
+                        new StringTokenizer(curLine, ";");
+
+                final String idAsString = tokenizer.nextToken();
+                final String ageAsString = tokenizer.nextToken();
+
+                long id = -1;
+                try {
+                    id = Long.parseLong(idAsString);
+                } catch (final NumberFormatException exception) {
+                    LOGGER.error("Cannot parse ID '{}'", idAsString);
+                }
+
+                int age = -1;
+                try {
+                    age = Integer.parseInt(ageAsString);
+                } catch (final NumberFormatException exception) {
+                    LOGGER.error("Cannot parse age '{}'", ageAsString);
+                }
+
+                if ((id > 0) && (age >= 0)) {
+                    final Human human = new Human();
+
+                    human.setAge(age);
+                    human.setId(id);
+
+                    returnValue.add(human);
+                }
+            }
+        } catch (final IOException exception) {
+            LOGGER.error("", exception);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+        return returnValue;
     }
 
     private Map<Integer, Double> readDeathRates(final String aPath) {
