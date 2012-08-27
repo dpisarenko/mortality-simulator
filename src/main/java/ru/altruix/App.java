7f84890f262386985ec7c70efdb6a145846acf0a
Dmitri Pisarenko <dp@altruix.co>
2012-08-27 16:12:32 +0200

    Attempt at bugfix: [ERROR] BUILD ERROR [INFO] ------------------------------------------------------------------------ [INFO] One or more required plugin parameters are invalid/missing for 'exec:java '
    [0] Inside the definition for plugin 'exec-maven-plugin' specify the following:
    
    <configuration>
      ...
      <mainClass>VALUE</mainClass>
    </configuration>
    
    -OR-
    
    on the command line, specify: '-Dexec.mainClass=VALUE'

--- src/main/java/ru/altruix/App.java
+++ src/main/java/ru/altruix/App.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
