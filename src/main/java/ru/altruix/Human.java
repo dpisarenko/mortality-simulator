23052b6d9e27f184e58a884060d483cfd3b490fa
Dmitri Pisarenko <dp@altruix.co>
2012-08-27 13:53:38 +0200

    Initial commit.

---
+++ src/main/java/ru/altruix/Human.java
@@ -0,0 +1,19 @@
+package ru.altruix;
+
+public class Human {
+    private long id;
+    private int age;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
