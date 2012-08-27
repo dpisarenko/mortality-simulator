d1d3927cde2ac6a73c245ddf573fbaa16bc857ee
Dmitri Pisarenko <dp@altruix.co>
2012-08-27 14:16:03 +0200

    Finished creating a scaffolding of App.java.

--- src/main/java/ru/altruix/App.java
+++ src/main/java/ru/altruix/App.java
@@ -1,36 +1,64 @@
 package ru.altruix;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
+public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
-    
-    public void run()
-    {
+
+    public void run() {
         LOGGER.info("Sample application");
-        
-        final DataReader reader = new DataReader();
-        reader.setPath("death-rates.csv");
-        reader.readData();
-        
-        final Map<Integer,Double> deathRatesByAge = reader.getData();
-        
-        
+
+        final Map<Integer, Double> deathRatesByAge =
+                readDeathRates("death-rates.csv");
+
+        final List<Human> people = readPeople("population.csv");
+
+        final DeathProbabilityCalculator calculator =
+                new DeathProbabilityCalculator();
+
+        calculator.setDeathRatesByAge(deathRatesByAge);
+        calculator.init();
+
+        for (final Human curPerson : people) {
+            final double experiment = Math.random();
+
+            final double[] range =
+                    calculator.getConditionalProbabilityRange(curPerson
+                            .getAge());
+
+            boolean death;
+            if ((range[0] >= experiment) && (experiment <= range[1])) {
+                death = true;
+            } else {
+                death = false;
+            }
+
+            LOGGER.debug("Age: {}, death: {}",
+                    new Object[] { curPerson.getAge(), death });
+        }
     }
-    public static void main( String[] args )
-    {
+
+    private List<Human> readPeople(final String aPath) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private Map<Integer, Double> readDeathRates(final String aPath) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void main(final String[] aArgs) {
         final App app = new App();
-        
+
         app.run();
     }
 }
---
+++ src/main/java/ru/altruix/DeathProbabilityCalculator.java
@@ -0,0 +1,22 @@
+package ru.altruix;
+
+import java.util.Map;
+
+public class DeathProbabilityCalculator {
+
+    public void init() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public double[] getConditionalProbabilityRange(int age) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setDeathRatesByAge(Map<Integer, Double> deathRatesByAge) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
