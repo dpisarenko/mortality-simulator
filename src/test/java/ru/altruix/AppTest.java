dd96485a8716457e5e220198cd8fefd8e0ddb30d
Dmitri Pisarenko <dp@altruix.co>
2012-08-27 16:16:29 +0200

    Created test case ru.altruix.AppTest.test() to catch following error:
    [WARNING]
    java.lang.reflect.InvocationTargetException
            at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
            at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.
    java:39)
            at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAcces
    sorImpl.java:25)
            at java.lang.reflect.Method.invoke(Method.java:597)
            at org.codehaus.mojo.exec.ExecJavaMojo$1.run(ExecJavaMojo.java:297)
            at java.lang.Thread.run(Thread.java:662)
    Caused by: java.lang.NullPointerException
            at ru.altruix.App.run(App.java:44)
            at ru.altruix.App.main(App.java:169)
            ... 6 more

--- src/test/java/ru/altruix/AppTest.java
+++ src/test/java/ru/altruix/AppTest.java
@@ -1,38 +1,25 @@
 package ru.altruix;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    public void test() {
+        try {
+            final App app = new App();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+            app.run();
+        } catch (final Throwable throwable) {
+            LOGGER.error("", throwable);
+            Assert.fail(throwable.getMessage());
+        }
     }
 }
